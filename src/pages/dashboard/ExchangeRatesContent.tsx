ce4189b72cdf59c86bcd44408777feb77d214223
matt <matt@emtech.com>
2022-01-16 17:10:37 -0500

    Add updated pages

--- src/pages/dashboard/ExchangeRatesContent.tsx
+++ src/pages/dashboard/ExchangeRatesContent.tsx
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
+import { InternationalMap } from '../../components/InternationalMap';
+import { CurrencyCode, CURRENCY_NAMES } from '../../lib/constants';
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,14 +8,54 @@ const Wrapper = styled.div`
   margin: 0 40px;
 `;
 
-const Header = styled.h2`
-  color: #2e2e2e;
-  font-weight: bold;
-  font-size: 28px;
-`;
+// TODO: Get this from an API
+const dummyExchangeResponse = {
+  EUR: 116.0,
+  USD: 101.62,
+  CAD: 80.9,
+  DOP: 1.75,
+};
 
-export const ExchangeRatesContent = (): JSX.Element => (
-  <Wrapper>
-    <Header>Exchange Rates</Header>
-  </Wrapper>
-);
+type CurrencyCoordinates = {
+  [K in CurrencyCode]?: [number, number];
+};
+
+// TODO: Get this from... ?
+const coordinates: CurrencyCoordinates = {
+  EUR: [4.469936, 50.503887],
+  USD: [-95.712891, 37.09024],
+  CAD: [-106.346771, 56.130366],
+  DOP: [-70.162651, 18.735693],
+};
+
+export const ExchangeRatesContent = ({
+  localCurrency,
+  exchangeCurrencies,
+}: {
+  localCurrency: CurrencyCode;
+  exchangeCurrencies: CurrencyCode[];
+}): JSX.Element => {
+  const theme = useTheme();
+
+  const mapColors = [
+    theme.colors.primary.green,
+    theme.colors.secondary.yellow,
+    theme.colors.primary.black,
+    theme.colors.secondary.red,
+    theme.colors.secondary.blue,
+  ];
+
+  const mapMarkers = exchangeCurrencies.map((curr, i) => ({
+    name: CURRENCY_NAMES[curr].name_formal,
+    color: mapColors[i % mapColors.length],
+    coordinates: coordinates[curr],
+    value: dummyExchangeResponse[curr],
+  }));
+  return (
+    <Wrapper>
+      <InternationalMap width={650} markers={mapMarkers} />
+    </Wrapper>
+  );
+};
+
+// base currency (3 letter code)
