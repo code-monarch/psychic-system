44859933f5c2665e53d6b4170c857eafd0604b4e
tobechukwuonuegbu <onuegbu.tobechukwu@yahoomail.com>
2023-01-10 19:43:20 +0400

    Update base urls

--- src/lib/apiConstants.ts
+++ src/lib/apiConstants.ts
@@ -2,5 +2,5 @@
 
 // *** Staging
 // export const MAIN_API = 'https://capkey.azurewebsites.net/dap/api/v1';
-export const MAIN_API = 'https://et-app-cap-stag-az.azurewebsites.net/dap/api/v1';
-export const SECURE_MAIN_API = 'https://digitalassets.staging.emtech.com/dcpinternal/v1';
+export const MAIN_API = 'https://et-app-cap-qa-az.azurewebsites.net/dap/api/v1';
+export const SECURE_MAIN_API = 'https://digitalassets.qa.emtech.com/dcpinternal/v1';
