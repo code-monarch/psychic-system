356b53f122a0b4a6323c00ec77c52ead37241873
Ajeet <ajeet@emtech.com>
2023-05-19 14:07:46 +0530

    Config: Edit endpoints

--- src/lib/apiConstants.ts
+++ src/lib/apiConstants.ts
@@ -1,6 +1,23 @@
 // eslint-disable-next-line import/prefer-default-export
 
-// *** QA
 // export const MAIN_API = 'https://capkey.azurewebsites.net/dap/api/v1';
+
+/** QA
+ * https://et-app-cap-qa-az.azurewebsites.net/dap/api/v1
+ * https://digitalassets.qa.emtech.com/dcpinternal/v1
+ *
+ * Staging
+ * https://et-app-cap-stag-az.azurewebsites.net/dap/api/v1
+ * https://digitalassets.staging.emtech.com/dcpinternal/v1
+ *
+ * Pilot
+ * https://et-cap-pilot-az.azurewebsites.net/dap/api/v1
+ * https://digitalassets.pilot.emtech.com/dcpinternal/v1
+ *
+ * Production
+ * https://et-sub-cap-prod-eastus.azurewebsites.net/dap/api/v1
+ * https://digitalassets.production.emtech.com/dcpinternal/v1
+ */
+
 export const MAIN_API = 'https://et-app-cap-qa-az.azurewebsites.net/dap/api/v1';
 export const SECURE_MAIN_API = 'https://digitalassets.qa.emtech.com/dcpinternal/v1';
