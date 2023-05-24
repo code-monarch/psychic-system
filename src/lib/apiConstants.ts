10fade14e42148fa3adbd25afb2ec5d7c13ae672
tobechukwuonuegbu <onuegbu.tobechukwu@yahoomail.com>
2023-05-24 15:40:45 +0400

    Setup env files and env vars in DAP-UI

--- src/lib/apiConstants.ts
+++ src/lib/apiConstants.ts
@@ -1,23 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
-
-// export const MAIN_API = 'https://capkey.azurewebsites.net/dap/api/v1';
-
-/** QA
- * https://et-app-cap-qa-az.azurewebsites.net/dap/api/v1
- * https://digitalassets.qa.emtech.com/dcpinternal/v1
- *
- * Staging
- * https://et-app-cap-stag-az.azurewebsites.net/dap/api/v1
- * https://digitalassets.staging.emtech.com/dcpinternal/v1
- *
- * Pilot
- * https://et-cap-pilot-az.azurewebsites.net/dap/api/v1
- * https://digitalassets.pilot.emtech.com/dcpinternal/v1
- *
- * Production
- * https://et-sub-cap-prod-eastus.azurewebsites.net/dap/api/v1
- * https://digitalassets.production.emtech.com/dcpinternal/v1
- */
-
-export const MAIN_API = 'https://et-app-cap-qa-az.azurewebsites.net/dap/api/v1';
-export const SECURE_MAIN_API = 'https://digitalassets.qa.emtech.com/dcpinternal/v1';
+export const MAIN_API = process.env.REACT_APP_API_HOST_CAP;
+export const SECURE_MAIN_API = process.env.REACT_APP_API_HOST_DAP;
